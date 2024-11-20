ff739d2b7e8498c10036d3784bda0e706b8fd4eb
Kathir K <128328717+KathirK12@users.noreply.github.com>
2024-11-19 20:07:22 -0500

    Update air-cownditioning-II.cpp
    trying again as somehow it has issues but on the complier I'm using it doesn't

--- air-cownditioning-II.cpp
+++ air-cownditioning-II.cpp
@@ -4,21 +4,6 @@ using ll = long long;
 
 vector<int> costs;
 map<ll, ll> ranges;
-map<int, vector<ll>> acs;
-
-void rec(int currentcost, map<ll, ll> rangesLeft, map<int, vector<ll>> acs) {
-    
-    if (rangesLeft.size() <= 0) {
-        
-        costs.push_back(currentcost);
-        
-    } else {
-        #include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-
-vector<int> costs;
-map<ll, ll> ranges;
 vector<array<ll, 4>> acs;
 vector<vector<ll>> subsets;
 
@@ -118,72 +103,4 @@ int main() {
     
     cout << *min_element(costs.begin(), costs.end()) << "\n";
     
-}
-        for (auto ac : acs) {
-            
-            map<int, vector<ll>> acs1 = acs;
-            map<ll, ll> rsl1 = rangesLeft;
-            int cc1 = currentcost + ac.second[3];
-            for (int i = ac.second[0]; i <= ac.second[1]; i++) {
-                
-                if (rsl1.find(i) != rsl1.end()) {
-                    
-                    rsl1[i] -= ac.second[2];
-                    
-                }
-            }
-            
-            map<ll, ll> rsl2 = rsl1;
-            for (auto range : rsl1) {
-                
-                if (range.second <= 0 && rsl2.find(range.first) != rsl2.end()) {
-                    
-                    rsl2.erase(range.first);
-                    
-                }
-                
-            }
-            
-            acs1.erase(ac.first);
-            
-            rec(cc1, rsl2, acs1);
-            
-        }
-        
-    }
-    
-}
-
-int main() {
-
-    int cows, numacs;
-    
-    cin >> cows >> numacs;
-    
-    for (int i = 0; i < cows; i++) {
-        
-        int start, end, lower;
-        
-        cin >> start >> end >> lower;
-        
-        for (int j = start; j <= end; j++) {
-            
-            ranges[j] = lower;
-            
-        }
-        
-    }
-    
-    for (int i = 0; i < numacs; i++) {
-        
-        acs[i].resize(4);
-        
-        cin >> acs[i][0] >> acs[i][1] >> acs[i][2] >> acs[i][3];
-        
-    }
-    
-    rec(0, ranges, acs);
-    
-    cout << *min_element(costs.begin(), costs.end()) << "\n";
-
 }
